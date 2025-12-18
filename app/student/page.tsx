bcfb35edc0497e0d5119db07c649dbbd594c8973
sadmimkarimi-lab <sadmimkarimi@gmail.com>
2025-12-19 03:02:08 +0330

    Update page.tsx

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -3,6 +3,7 @@ import StudentClient from "./student-client";
 
 export const dynamic = "force-dynamic";
 
+
 export default function StudentPage() {
   return (
     <Suspense fallback={<div style={{ padding: 24, textAlign: "center" }}>در حال بارگذاری…</div>}>
